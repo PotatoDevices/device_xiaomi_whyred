884a7ed0b1dc1075245aebdbc7444316b8a77747
simrat39 <simrats169169@gmail.com>
2020-01-28 11:12:15 +0530

    whyred: prop: Reorder vendor_prop

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,23 +1,44 @@
+# Misc
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.job_delay=true \
     persist.sys.mcd_config_file=/system/etc/mcd_default.conf \
     vendor.video.disable.ubwc=1 \
-    ro.hardware.nfc_nci=nqx.default \
-    vendor.hw.fm.init=0 \
-    persist.radio.multisim.config=dsds \
     persist.vendor.qcomsysd.enabled=1 \
-    ro.dalvik.vm.native.bridge=0 \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.sib16_support=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine \
+    sys.autosuspend.timeout=500000 \
+
+# NTP
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.backup.ntpServer=0.pool.ntp.org \
-    sys.vendor.shutdown.waittime=500 \
-    ro.build.shutdown_timeout=0 \
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=nqx.default \
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp \
-    ro.opengles.version=196610 \
-    vendor.qcom.bluetooth.soc=cherokee \
+
+# QTI 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# Shutdown
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.shutdown.waittime=500 \
+    ro.build.shutdown_timeout=0
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.sensors.facing=false \
+    ro.vendor.sensors.cmc=false \
+    ro.vendor.sdk.sensors.gestures=false
+
+# (audio) MIUI / Misc
+PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     vendor.audio_hal.period_size=192 \
     ro.vendor.audio.sdk.fluencetype=fluence \
@@ -30,7 +51,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.video=true \
     vendor.audio.offload.track.enable=false \
     audio.deep_buffer.media=true \
-    vendor.voice.path.for.pcm.voip=true \
     vendor.audio.offload.multiaac.enable=true \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
@@ -41,36 +61,34 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.safx.pbe.enabled=true \
     vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.flac.sw.decoder.24bit=true \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
-		persist.bluetooth.a2dp_offload.cap=sbc-aptx-aptxhd-aac \
     vendor.audio.use.sw.alac.decoder=true \
-		ro.vendor.bluetooth.wipower=false \
     vendor.audio.use.sw.ape.decoder=true \
     vendor.audio.hw.aac.encoder=true \
-    vendor.fm.a2dp.conc.disabled=true \
     vendor.audio.noisy.broadcast.delay=600 \
     persist.vendor.audio.hifi.int_codec=true \
     vendor.audio.offload.pstimeout.secs=3 \
-    ro.af.client_heap_size_kbyte=7168 \
-    persist.vendor.audio.hw.binder.size_kbyte=1024 \
     ro.vendor.audio.sos=true \
     ro.vendor.audio.soundtrigger=nuance \
-    ro.vendor.audio.soundtrigger.lowpower=false \
-    ro.vendor.sensors.facing=false \
-    ro.vendor.sensors.cmc=false \
-    ro.vendor.sdk.sensors.gestures=false \
-    rild.libpath=/system/vendor/lib64/libril-qc-hal-qmi.so \
-    sys.autosuspend.timeout=500000 \
-    drm.service.enabled=true
+    ro.af.client_heap_size_kbyte=7168 \
+    vendor.voice.path.for.pcm.voip=true \
+    ro.vendor.audio.soundtrigger.lowpower=false
 
-# Enable headset calibration
+# (audio) FM
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.hw.fm.init=0 \
+    vendor.fm.a2dp.conc.disabled=true \
+    vendor.audio.feature.fm.enable=true
+
+# (audio) Enable headset calibration
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.volume.headset.gain.depcal=true
 
-# Add dynamic feature flags here
+# (audio) Add dynamic feature flags here
 PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.feature.a2dp_offload.enable=false \
     vendor.audio.feature.afe_proxy.enable=true \
     vendor.audio.feature.anc_headset.enable=true \
+    vendor.audio.feature.audio_sphere.enable=true \
     vendor.audio.feature.battery_listener.enable=false \
     vendor.audio.feature.compr_cap.enable=false \
     vendor.audio.feature.compress_in.enable=false \
@@ -86,7 +104,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.feature.external_speaker.enable=false \
     vendor.audio.feature.external_speaker_tfa.enable=false \
     vendor.audio.feature.fluence.enable=true \
-    vendor.audio.feature.fm.enable=true \
     vendor.audio.feature.hdmi_edid.enable=true \
     vendor.audio.feature.hdmi_passthrough.enable=true \
     vendor.audio.feature.hfp.enable=true \
@@ -111,60 +128,34 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.feature.audiozoom.enable=false \
     vendor.audio.feature.snd_mon.enable=false
 
-# Enable dualmic fluence for voice communication
+# (audio) Enable dualmic fluence for voice communication
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.voicecomm=true
 
-# Enable AAC frame ctl for A2DP sinks
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.bt.aac_frm_ctl.enabled=true
-
-# Codec2 switch
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.media.codec2=2
-
-# Media
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.stagefright.omx_default_rank.sw-audio=1 \
-    debug.stagefright.omx_default_rank=0
-
-# Rendering
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.enable.sglscale=1 \
-    debug.egl.hw=1 \
-    debug.sf.disable_hwc=0 \
-    debug.sf.recomputecrop=0 \
-    persist.hwc.ptor.enable=true \
-    debug.sf.gpu_comp_tiling=1
-
-# Set speaker protection cal tx path sampling rate to 48k
+# (audio) Set speaker protection cal tx path sampling rate to 48k
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.spkr_prot.tx.sampling_rate=48000
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
-
+# (audio)
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio_hal.in_period_size=144 \
     vendor.audio_hal.period_multiplier=3 \
     vendor.audio.adm.buffering.ms=12
 
-# Enable backpressure for GL comp
+# (audio) h/w memory tuning 
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_gl_backpressure=1
+    persist.vendor.audio.hw.binder.size_kbyte=1024
 
-# VoLTE/IMS
+# (media) Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0
+
+# (media) Codec2 switch
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.media.codec2=2
+
+# (ril) VoLTE/IMS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.ims_volte_enable=1 \
     persist.dbg.volte_avail_ovr=1 \
@@ -174,16 +165,63 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.data_ltd_sys_ind=1 \
     persist.vendor.radio.procedure_bytes=SKIP \
     persist.radio.calls.on.ims=1 \
-    persist.dbg.wfc_avail_ovr=1
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.dbg.wfc_avail_ovr=1 \
+    rild.libpath=/system/vendor/lib64/libril-qc-hal-qmi.so \
 
+# (bt) Enable AAC frame ctl for A2DP sinks
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.audio.hw.binder.size_kbyte=1024
+    persist.vendor.bt.aac_frm_ctl.enabled=true
 
-# Provides overrides to configure the Dalvik heap for a 4GB phone
+# (bt) Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.qcom.bluetooth.soc=cherokee \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+	persist.bluetooth.a2dp_offload.cap=sbc-aptx-aptxhd-aac \
+    ro.vendor.bluetooth.wipower=false
+
+# Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.6 \
     dalvik.vm.heapminfree=8m \
-    dalvik.vm.heapmaxfree=16m
+    dalvik.vm.heapmaxfree=16m \
+    ro.dalvik.vm.native.bridge=0
+
+# (graphics) SurfaceFlinger 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+
+# (graphics) SF Offsets
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000
+
+# (graphics) Rendering
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.enable.sglscale=1 \
+    debug.egl.hw=1 \
+    debug.sf.disable_hwc=0 \
+    debug.sf.recomputecrop=0 \
+    persist.hwc.ptor.enable=true \
+    debug.sf.gpu_comp_tiling=1
+
+# (graphics) Enable backpressure for GL comp
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_gl_backpressure=1
+
+# (graphics) Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196610 \
