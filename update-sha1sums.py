5dee022c9f1f42289d4b9bace4fdd215a9eda973
dianlujitao <dianlujitao@lineageos.org>
2020-01-31 12:19:35 +0800

    whyred: update-sha1sums: Better handle line breaks
    Change-Id: I15cfda82a5bc6f05c0d7b9db209064ba498dac11

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,64 +19,57 @@
 from hashlib import sha1
 import sys
 
-device = "whyred"
-vendor = "xiaomi"
+device='whyred'
+vendor='xiaomi'
 
-lines = [line for line in open("proprietary-files.txt", "r")]
-vendorPath = "../../../vendor/" + vendor + "/" + device + "/proprietary"
+with open('proprietary-files.txt', 'r') as f:
+    lines = f.read().splitlines()
+vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
 needSHA1 = False
 
 
 def cleanup():
     for index, line in enumerate(lines):
-        # Remove '\n' character
-        line = line[:-1]
-
         # Skip empty or commented lines
-        if len(line) == 0 or line[0] == "#":
+        if len(line) == 0 or line[0] == '#':
             continue
 
         # Drop SHA1 hash, if existing
-        if "|" in line:
-            line = line.split("|")[0]
-            lines[index] = "%s\n" % (line)
+        if '|' in line:
+            line = line.split('|')[0]
+            lines[index] = '%s' % (line)
 
 
 def update():
     for index, line in enumerate(lines):
-        # Remove '\n' character
-        line = line[:-1]
-
         # Skip empty lines
         if len(line) == 0:
             continue
 
         # Check if we need to set SHA1 hash for the next files
-        if line[0] == "#":
-            needSHA1 = " - from" in line
+        if line[0] == '#':
+            needSHA1 = (' - from' in line)
             continue
 
         if needSHA1:
             # Remove existing SHA1 hash
-            line = line.split("|")[0]
-            filePath = line.split(":")[1] if len(line.split(":")) == 2 else line
+            line = line.split('|')[0]
+            filePath = line.split(':')[1] if len(
+                line.split(':')) == 2 else line
 
-            if filePath[0] == "-":
-                file = open("%s/%s" % (vendorPath, filePath[1:]), "rb").read()
+            if filePath[0] == '-':
+                file = open('%s/%s' % (vendorPath, filePath[1:]), 'rb').read()
             else:
-                file = open("%s/%s" % (vendorPath, filePath), "rb").read()
+                file = open('%s/%s' % (vendorPath, filePath), 'rb').read()
 
             hash = sha1(file).hexdigest()
-            lines[index] = "%s|%s\n" % (line, hash)
+            lines[index] = '%s|%s' % (line, hash)
 
 
-if len(sys.argv) == 2 and sys.argv[1] == "-c":
+if len(sys.argv) == 2 and sys.argv[1] == '-c':
     cleanup()
 else:
     update()
 
-with open("proprietary-files.txt", "w") as file:
-    for line in lines:
-        file.write(line)
-
-    file.close()
+with open('proprietary-files.txt', 'w') as file:
+    file.write('\n'.join(lines) + '\n')
